cbf213e9f53d025f58e056baab5ccdd26aa38de2
MD Tariqul Islam <ittarek551@gmail.com>
2023-06-08 05:51:09 +0600

    registration page styles

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,37 +1,77 @@
 import React from "react";
-// import './Login.css'
-import loginImg from '../../assets/login/login.jpg'
+import "./Login.css";
+import loginImg from "../../assets/login/login.jpg";
 import Container from "../../Componets/Container";
 import SocailLogin from "../../Componets/Socail/SocailLogin";
+import { useForm } from "react-hook-form";
+import './Login.css'
+import { Link } from "react-router-dom";
 const Login = () => {
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <Container>
-  <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-        <div className='hidden sm:block'>
-            <img className='w-full h-full object-cover' src={loginImg} alt="" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full mb-10">
+        <div className="hidden sm:block">
+          <img className="w-full h-full object-cover" src={loginImg} alt="" />
         </div>
 
-        <div className='bg-gray-900 flex flex-col justify-center '>
-            <form className='max-w-[400px] w-full mx-auto rounded-lg  p-8 px-8'>
-                <h2 className='text-4xl dark:text-white font-bold text-center'>SIGN IN</h2>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Username</label>
-                    <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
-                </div>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Password</label>
-                    <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" />
-                </div>
-                <div className='flex justify-between text-gray-400 py-2'>
-                    <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
-                    <p>Forgot Password</p>
-                </div>
-                <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>SIGNIN</button>
-                
-            </form>
-            <SocailLogin></SocailLogin>
+        <div className="bg-gray-900 flex flex-col justify-center ">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="max-w-[400px] w-full mx-auto rounded-lg  p-8 px-8"
+          >
+            <h2 className="text-4xl dark:text-white font-bold text-center">
+              SIGN IN
+            </h2>
+            <div className="flex flex-col text-gray-400 py-2">
+              <label>Email</label>
+              <input
+                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                type="text" name="email"
+                {...register("email", { required: true })}
+              />    {errors.email && (
+                <span className="text-red-600">Email field is required</span>
+              )}
+            </div>
+            <div className="flex flex-col text-gray-400 py-2">
+              <label>Password</label>
+              <input
+                className="p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                type="password" name="password"
+                {...register("password", { required: true })}
+              />
+               {errors.password && (
+                <span className="text-red-600">Password field is required</span>
+              )}
+            </div>
+            <div className="flex justify-between text-gray-400 py-2">
+              <p className="flex items-center">
+                <input className="mr-2" type="checkbox" /> Remember Me
+              </p>
+              <p>Forgot Password</p>
+            </div>
+            <button className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg">
+              {" "}
+              <input type="submit" value="SIGNIN" />
+            </button>
+            <span className="text-white">New To Language Center? Please ! <Link to='/register' className="text-green-400">SignUp</Link></span>
+          </form>
+          <SocailLogin></SocailLogin>
+
+          {errors.exampleRequired && (
+            <span className="text-red-400">This field is required</span>
+          )}
         </div>
-    </div>
+      </div>
     </Container>
   );
 };
--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -1,52 +1,150 @@
-import './Register.css'
+import { Link } from "react-router-dom";
+import "./Register.css";
 
+import { useForm } from "react-hook-form";
 const Register = () => {
-          return (
-                    <div>
-                              <div class="py-10 img-gradient">
-                    <div class="container mx-auto">
-                      <div class="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
-                        <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-image" >
-                          <h1 class="text-white text-3xl mb-3">Welcome</h1>
-                          <div>
-                            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suspendisse aliquam varius rutrum purus maecenas ac <a href="#" class="text-purple-500 font-semibold">Learn more</a></p>
-                          </div>
-                        </div>
-                        <div class="w-full lg:w-1/2 py-16 px-12">
-                          <h2  class="text-3xl mb-4">Register</h2>
-                          <p class="mb-4">
-                            Create your account. It’s free and only take a minute
-                          </p>
-                          <form action="#">
-                            <div class="grid grid-cols-2 gap-5">
-                              <input type="text" placeholder="Firstname" class="border border-gray-400 py-1 px-2"/>
-                              <input type="text" placeholder="Surname" class="border border-gray-400 py-1 px-2"/>
-                            </div>
-                            <div class="mt-5">
-                              <input type="text" placeholder="Email" class="border border-gray-400 py-1 px-2 w-full"/>
-                            </div>
-                            <div class="mt-5">
-                              <input type="password" placeholder="Password" class="border border-gray-400 py-1 px-2 w-full"/>
-                            </div>
-                            <div class="mt-5">
-                              <input type="password" placeholder="Confirm Password" class="border border-gray-400 py-1 px-2 w-full"/>
-                            </div>
-                            <div class="mt-5">
-                              <input type="checkbox" class="border border-gray-400"/>
-                              <span>
-                                I accept the <a href="#" class="text-purple-500 font-semibold">Terms of Use</a> &  <a href="#" class="text-purple-500 font-semibold">Privacy Policy</a> 
-                              </span>
-                            </div>
-                            <div class="mt-5">
-                              <button class="w-full bg-purple-500 py-3 text-center text-white">Register Now</button>
-                            </div>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                    </div>
-          );
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <div className="py-10 img-gradient mb-10">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+            <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-image">
+              <h1 className="text-white text-3xl mb-3">Welcome</h1>
+              <div>
+                <p className="text-white">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aenean suspendisse aliquam varius rutrum purus maecenas ac{" "}
+                  <Link to="" className="text-purple-500 font-semibold">
+                    Learn more
+                  </Link>
+                </p>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 py-16 px-12">
+              <h2 className="text-3xl mb-4">Register</h2>
+              <p className="mb-4">
+                Create your account. It’s free and only take a minute
+              </p>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="">
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    {...register("name", { required: true })}
+                    className="border border-gray-400 py-1 px-2 w-full"
+                  />{" "}
+                  {errors.name && (
+                    <span className="text-red-600">Name field is required</span>
+                  )}
+                </div>
+                <div className="mt-5">
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    {...register("email", { required: true })}
+                    className="border border-gray-400 py-1 px-2 w-full"
+                  />{" "}
+                  {errors.email && (
+                    <span className="text-red-600">
+                      Email field is required
+                    </span>
+                  )}
+                </div>
+                <div className="mt-5">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    {...register("password", {
+                      required: true,
+                      minLength: 6,
+
+                      pattern:
+                        /(?=.*[A-Z].)(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                    })}
+                    className="border border-gray-400 py-1 px-2 w-full"
+                  />{" "}
+                  {errors.password?.type === "required" && (
+                    <span className="text-red-600">
+                      Password field is required
+                    </span>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <span className="text-red-600">
+                      Password must be 6 character
+                    </span>
+                  )}
+                  {errors.password?.type === "pattern" && (
+                    <span className="text-red-600">
+                      Password must be 1 spacial character 1 uppercase 1 number
+                      1 lowercase{" "}
+                    </span>
+                  )}
+                </div>
+                <div className="mt-5">
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    {...register("confirmPassword", { required: true })}
+                    className="border border-gray-400 py-1 px-2 w-full"
+                  />
+                  {errors.confirmPassword && (
+                    <span className="text-red-600">
+                      Confirm Password is required
+                    </span>
+                  )}
+                </div>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    {...register("photoURL", { required: true })}
+                    placeholder="Photo URL"
+                    className="input input-bordered"
+                  />
+                  {errors.photoURL && (
+                    <span className="text-red-600">Photo URL is required</span>
+                  )}
+                </div>
+                <div className="mt-5">
+                  <input type="checkbox" className="border border-gray-400" />
+                  <span>
+                    I accept the{" "}
+                    <Link to="" className="text-purple-500 font-semibold">
+                      Terms of Use
+                    </Link>{" "}
+                    &{" "}
+                    <Link to="" className="text-purple-500 font-semibold">
+                      Privacy Policy
+                    </Link>
+                  </span>
+                </div>
+                <div className="mt-5">
+                  <button className="w-full bg-purple-500 py-3 text-center text-white">
+                    <input type="submit" value="Register Now" />
+                  </button>
+                </div>
+                <span>
+                  Already Have an Account ?{" "}
+                  <Link to="/login" className="text-purple-500">
+                    Login
+                  </Link>
+                </span>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
