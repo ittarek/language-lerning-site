699650813f3b37db939ac6d16ac380e51ff717d1
Md Tariqul Islam <ittarek551@gmail.com>
2026-01-24 14:38:36 +0600

    remove unusale code

--- src/LayOut/MainLayOut.jsx
+++ src/LayOut/MainLayOut.jsx
@@ -1,8 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Shared_pages/Header/Navbar/Navbar';
 import Footer from '../Shared_pages/Footer/Footer';
-import { useContext, useEffect } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
+import {  useEffect } from 'react';
 import AOS from 'aos';
 import { ScrollToTop } from '../Components/Shared/ScrollToTop';
 
