506752e86cfb3ece31dda6dde44ddf69cc256351
Md Tariqul Islam <ittarek551@gmail.com>
2026-01-29 12:33:12 +0600

    add scrollTo Top

--- src/LayOut/MainLayOut.jsx
+++ src/LayOut/MainLayOut.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Shared_pages/Header/Navbar/Navbar';
 import Footer from '../Shared_pages/Footer/Footer';
-import {  useEffect } from 'react';
+import { useEffect } from 'react';
 import AOS from 'aos';
-import { ScrollToTop } from '../Components/Shared/ScrollToTop';
+import {ScrollToTop} from '../Components/Shared/ScrollToTop';
 
 const MainLayOut = () => {
   const pathName = window.location.pathname;
@@ -17,7 +17,8 @@ const MainLayOut = () => {
   }, []);
   return (
     <>
-      {pathName !== '/' && <ScrollToTop />}
+      {/* {pathName !== '/' && <ScrollToTop />} */}
+      <ScrollToTop />
       <Navbar />
       <div className="md:min-h-[calc(100vh-140px)]">
         {/* {isLoading ? <PageLoadingSpinner /> :         <Outlet />} */}
